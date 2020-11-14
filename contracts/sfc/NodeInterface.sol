c844c2b8f838110e96be4473bff195e828720d77
Adam <boudjemaa.adam@gmail.com>
2020-11-14 15:57:43 +0100

    Remove duplicates and unused functions, events and variables

--- contracts/sfc/NodeInterface.sol
+++ contracts/sfc/NodeInterface.sol
@@ -11,10 +11,10 @@ interface NodeInterface {
 
     event UpdatedValidatorWeight(uint256 indexed validatorID, uint256 weight);
 
-    event UpdatedGasPowerAllocationRate(uint256 short, uint256 long);
-    event UpdatedMinGasPrice(uint256 minGasPrice);
+//    event UpdatedGasPowerAllocationRate(uint256 short, uint256 long);
+//    event UpdatedMinGasPrice(uint256 minGasPrice);
 
     function _deactivateValidator(uint256 validatorID, uint256 status) external;
-    function _sealEpochValidators(uint256[] calldata nextValidatorIDs) external;
-    function _sealEpoch(uint256[] calldata offlineTimes, uint256[] calldata offlineBlocks, uint256[] calldata uptimes, uint256[] calldata originatedTxsFee) external;
+    function sealEpochValidators(uint256[] calldata nextValidatorIDs) external;
+    function sealEpoch(uint256[] calldata offlineTimes, uint256[] calldata offlineBlocks, uint256[] calldata uptimes, uint256[] calldata originatedTxsFee) external;
 }
