08e9722d3cf96fa045575a297850798a34df0037
Egor Lysenko <uprendis@gmail.com>
2021-02-23 10:47:45 +0700

    version up

--- test/SFC.js
+++ test/SFC.js
@@ -220,7 +220,7 @@ contract('SFC', async ([firstValidator, secondValidator, thirdValidator]) => {
             });
 
             it('Returns the version of the current implementation', async () => {
-                expect((await this.sfc.version()).toString()).to.equals('0x323032');
+                expect((await this.sfc.version()).toString()).to.equals('0x333030');
             });
 
             it('Should create a Validator and return the ID', async () => {
