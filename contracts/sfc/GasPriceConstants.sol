7be31eeca41844cff8a032b030bbbfd8e6984b1d
Mike-CZ <m1k3cz@pm.me>
2024-10-22 18:50:44 +0200

    Add custom errors for `ConstantsManager` (#75)

--- contracts/sfc/GasPriceConstants.sol
+++ contracts/sfc/GasPriceConstants.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-import "../common/Decimal.sol";
+import {Decimal} from "../common/Decimal.sol";
 
 library GP {
     function trimGasPriceChangeRatio(uint256 x) internal pure returns (uint256) {
