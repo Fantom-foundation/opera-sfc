883717f7678d883d33be9be4b63a246e02193ddd
Mike-CZ <pelanek.lukas@seznam.cz>
2024-10-25 10:31:17 +0200

    Move interfaces into separate directory

--- contracts/sfc/NetworkInitializer.sol
+++ contracts/sfc/NetworkInitializer.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-import {SFCI} from "./SFCI.sol";
+import {ISfc} from "../interfaces/ISfc.sol";
 import {NodeDriver, NodeDriverAuth} from "./NodeDriver.sol";
 import {ConstantsManager} from "./ConstantsManager.sol";
 import {Decimal} from "../common/Decimal.sol";
@@ -40,6 +40,6 @@ contract NetworkInitializer {
         consts.updateGasPriceBalancingCounterweight(3600);
         consts.transferOwnership(_owner);
 
-        SFCI(_sfc).initialize(sealedEpoch, totalSupply, _auth, _lib, address(consts), _owner);
+        ISfc(_sfc).initialize(sealedEpoch, totalSupply, _auth, _lib, address(consts), _owner);
     }
 }
--- contracts/test/StubEvmWriter.sol
+++ contracts/test/StubEvmWriter.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-import {IEvmWriter} from "../interfaces/IEVMWriter.sol";
+import {IEvmWriter} from "../interfaces/IEvmWriter.sol";
 
 contract StubEvmWriter is IEvmWriter {
     function setBalance(address acc, uint256 value) external {}
