7afed4ad837bfe3871b1ffe1433b5c116d7af45d
mike <m1k3cz@pm.me>
2024-12-13 17:56:06 +0100

    Replace assembly with high-level constructs

--- contracts/sfc/NodeDriverAuth.sol
+++ contracts/sfc/NodeDriverAuth.sol
@@ -171,19 +171,10 @@ contract NodeDriverAuth is OwnableUpgradeable, UUPSUpgradeable {
     }
 
     function isContract(address account) internal view returns (bool) {
-        uint256 size;
-        // solhint-disable-next-line no-inline-assembly
-        assembly {
-            size := extcodesize(account)
-        }
-        return size > 0;
+        return account.code.length > 0;
     }
 
     function _getCodeHash(address addr) internal view returns (bytes32) {
-        bytes32 codeHash;
-        assembly {
-            codeHash := extcodehash(addr)
-        }
-        return codeHash;
+        return addr.codehash;
     }
 }
